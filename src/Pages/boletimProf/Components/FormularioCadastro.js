0f2c3a17c42708a490db5ca18cf342159e69bd0c
Matheus Lima <mbl@discente.ifpe.edu.br>
2021-12-14 13:36:35 -0300

    14/12 - 13:36

--- src/Pages/boletimProf/Components/FormularioCadastro.js
+++ src/Pages/boletimProf/Components/FormularioCadastro.js
@@ -44,71 +44,50 @@ const FormularioCadastro = (props) =>{
     }
 
     return (
-        <form autoComplete="off" onSubmit={manipuladorFormEnvio}>
+        <form onSubmit={manipuladorFormEnvio}>
         <div className="containerNotas">
-            <div className="">
-                <div className="">
-                    <div className="">
-                    
-                    </div>
-                </div>
-
+           
                 <input className="form-control" type="text" placeholder="Digite o Nome do Aluno" name="Nome" value={values.Nome}
                     
                 onChange={manipuladorOnChange}/>
-            </div>
 
         
-               
-                <div className="">
-                    <div className="">
-                        <div className="">
-                        
-                        </div>
-                    </div>
+                    
 
                     <input className="form-control"  type="number" placeholder="Digite a NotaUm" name="NotaUm" value={values.NotaUm}
                     onChange={manipuladorOnChange}/>
-                </div>
-
-                <div className="">
                     <div className="">
-                        <div className="">
-                          
-                        </div>
-                    </div>
+                    
                     <input className="form-control" type="number" placeholder="Digite a NotaDois" name="NotaDois" value={values.NotaDois}
                     onChange={manipuladorOnChange}/>
                 </div>
             
 
             <div className="">
-                    <div className="">
-                    <div className="">
-                        
-                       
-                    </div>
-                </div>
-
+                    
                 <input className="form-control" type="number" placeholder="Digite a NotaTres" name="NotaTres" value={values.NotaTres}
                 onChange={manipuladorOnChange}/>
             </div>
            
             <div className="">
-                    <div className="">
-                        <div className="">
-                       
-                    </div>
-                </div>
+                    
 
-                <input className="form-control" type="number" placeholder="Digite a NotaQuatro" name="NotaQuatro" value={values.NotaQuatro}
+             <input className="form-control" type="number" placeholder="Digite a NotaQuatro" name="NotaQuatro" value={values.NotaQuatro}
                 onChange={manipuladorOnChange}/>
             </div>
             <div className="btnCadastrar">
                 <input type="submit" value={props.idAtual == '' ? 'Cadastrar' : 'atualizar'} className="btn btn-secondary btn-block" />
             </div>
-</div>
+
+                </div>
+
+                
         </form>
+            
+
+        
+               
+               
 
     )
 }
